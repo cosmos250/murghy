359ebcde4068d4ae8dfd9b091d3c281321410cd0
cosmos250 <cosmos250.xy@gmail.com>
2014-11-16 10:28:41 +0800

    add SX.java

---
+++ src/main/java/s/b/xy/SX.java
@@ -0,0 +1,16 @@
+package s.b.xy;
+
+import android.util.Log;
+
+/**
+ * @author xingyong@360.cn
+ * @ClassName: SX
+ * @Description: haha
+ * @date 14/11/16 上午10:27
+ */
+public class SX {
+
+    public void test() {
+        Log.d("SB", "XY shi SB");
+    }
+}
